3227d88c3b2dfbdd50337beafc67a3cc91af07f5
tarunb12 <tbelani12@gmail.com>
2019-02-13 01:37:45 -0500

    each line in test.txt treated as its own test

--- rpn.ml
+++ rpn.ml
@@ -25,10 +25,7 @@ let eval_op op num_1 num_2 =
   | _ -> Float.nan;;
 
 let rec eval_expr expr float_stack =
-  if List.length expr <= 0 then
-    if List.length float_stack == 1 then (List.hd float_stack)
-    else Float.nan 
-  else
+  if List.length expr > 0 then
     let token = List.hd expr in
       let is_float t = try ignore (float_of_string t); true with _ -> false in
         let new_expr = remove expr in
@@ -43,7 +40,10 @@ let rec eval_expr expr float_stack =
                     let pop_then_push stack = push (pop (pop stack)) in
                       eval_expr new_expr (pop_then_push float_stack (eval_op token num_1 num_2))
               else Float.nan
-            else Float.nan;;
+            else Float.nan
+  else
+    if List.length float_stack == 1 then (List.hd float_stack)
+    else Float.nan;;
 
 let read_expr expression =
   let spaceSplit = Str.(split(regexp " +")) in
@@ -58,6 +58,6 @@ let classify_float result =
 
 let input = read_line () in
   let result = read_expr input in
-    match input with
+    match (String.trim input) with
     | "" -> Printf.printf ""
     | _ -> classify_float result;;
--- test.ml
+++ test.ml
@@ -1,25 +1,21 @@
 open OUnit2
 
+let rec tests index test_list ic_test ic_result =
+  try
+    let expr = input_line ic_test in
+      let result = input_line ic_result in
+        let test_name = "test" ^ (string_of_int index) in
+          let add_to_test_list t_list = List.append test_list t_list in
+            let test_assertion = fun test_ctxt -> assert_equal (Rpn.read_expr expr) (float_of_string (String.trim result)) in
+              let new_test = test_name >:: test_assertion in
+                tests (index + 1) (add_to_test_list [new_test]) ic_test ic_result;
+  with End_of_file -> test_list;;
 
-let test_fixture = "Rpn" >:::
-[
-  "test.txt" >:: (fun test_ctxt ->
-  let ic_t = open_in "test.txt" in
-    let ic_r = open_in "result.txt" in
-      try
-        while true do
-          let expr = input_line ic_t in
-            let result = input_line ic_r in
-            assert_equal (Rpn.read_expr expr) (float_of_string result)
-        done;
-    with End_of_file ->
-      close_in ic_t;
-      close_in ic_r;
-  );
-]
-let () = run_test_tt_main test_fixture;;
-
-(* 
-ocamlfind ocamlc str.cma -o b -package ounit -linkpkg -g rpn.ml test.ml
-ITERATE THROUGH TESTS WITH TEST.TXT AND RESULT.TXT
-*)
+let ic_test = open_in "test.txt" in
+  let ic_result = open_in "result.txt" in
+    try
+      let test_fixture = "RPN" >::: (tests 0 [] ic_test ic_result) in
+        run_test_tt_main test_fixture
+    with _ -> 
+      close_in ic_test;
+      close_in ic_result;;
